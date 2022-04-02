5c020bee6fa0f11f78bad676e97814c095d3fb04
gnehsheng <gnehsheng@gmail.com>
2022-04-02 16:05:23 +0800

    created delete

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -4,6 +4,14 @@ import { BACKEND } from "../utils/utils";
 export default function HolidayList() {
     const [holidays, setHolidays] = useState([]);
 
+    const deleteHoliday = (id) => {
+        fetch("/holidays/" + id, {
+          method: "DELETE",
+        }).then((response) => {
+          setHolidays(holidays.filter((day) => day._id !== id));
+        });
+      };
+
     useEffect(() => {
         fetch(`${BACKEND}/api/holidays/seed`)
             .then((response) => response.json())
@@ -17,7 +25,10 @@ export default function HolidayList() {
                     {holidays.map((holiday) => {
                         return (
                             <tr key={holiday.id}>
-                                <td><a href='#'>{holiday.name}</a> </td>
+                                <td>
+                                    <a href='#'>{holiday.name}</a> <br/>
+                                    <td onClick={() => deleteHoliday(holiday._id)}>Delete</td>
+                                </td>
                             </tr>
                         );
                     })}
