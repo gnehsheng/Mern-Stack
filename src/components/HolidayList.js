8baf60489810a7d3617bd326c6155fa4b29539cb
gnehsheng <gnehsheng@gmail.com>
2022-04-02 15:56:45 +0800

    created table for results

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -1,7 +1,28 @@
-export default function Holidayli() {
+import { useEffect, useState } from "react";
+import { BACKEND } from "../utils/utils";
+
+export default function HolidayList() {
+    const [holidays, setHolidays] = useState([]);
+
+    useEffect(() => {
+        fetch(`${BACKEND}/api/holidays/seed`)
+            .then((response) => response.json())
+            .then((data) => setHolidays(data));
+    }, []);
+
     return (
         <>
-            <ul><li>HolidayList</li></ul>
+            <table>
+                <tbody>
+                    {holidays.map((holiday) => {
+                        return (
+                            <tr key={holiday.id}>
+                                <td><a href='#'>{holiday.name}</a> </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </>
     )
 }
